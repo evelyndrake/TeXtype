c0042396ab25fb5587f805f128094be3f8aa75dc
evelyndrake <evelyndrake90@gmail.com>
2025-03-18 11:10:27 -0400

    Refactored comparisons to use MathML, UI changes, still some comparison issues

--- textype/src/InputField.tsx
+++ textype/src/InputField.tsx
@@ -3,53 +3,73 @@ import { useState } from 'react'
 import './App.css'
 import './index.css'
 import Katex from './Katex'
+import Temml from 'temml'
 
 interface InputFieldProps {
-	expression: string
-	onMatch: () => void
+  expression: string
+  onMatch: () => void
 }
 
 function InputField({ expression, onMatch }: InputFieldProps) {
-	// State to hold the user input
-	const [userInput, setUserInput] = useState("")
-	const [showPopup, setShowPopup] = useState(false);
+  // State to hold the user input
+  const [userInput, setUserInput] = useState("")
+  const [showPopup, setShowPopup] = useState(false);
 
-	// Function to check if the user input matches the expression
-	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const input = e.target.value;
-		setUserInput(input);
-		if (input.replace(/\s/g, "") === expression.replace(/\s/g, "")) {
-			// If the input matches the expression, call the onMatch function
-			onMatch();
-			console.log("Matched");
-			setUserInput("");
-			setShowPopup(true);
-		} else if (showPopup) {
-			setShowPopup(false);
-		}
-	}
-	return (
-		<>
-			<h2>Type the expression above</h2>
-			<input
-				type="text"
-				value={userInput}
-				onChange={handleChange}
-				placeholder="Type your expression here"
-			/>
-			{showPopup && (
-				<div className="neon-text">
-					<p>Correct!</p>
-				</div>
-			)}
-			<h2>Your input</h2>
-			<Katex
-				texExpression={userInput}
-				className="katex"
-			/>
+  // Function to check if the user input matches the expression
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const input = e.target.value;
+    setUserInput(input);
+    if (compareExpressions(expression, input)) {
+      // if (input.replace(/\s/g, "") === expression.replace(/\s/g, "")) {
+      // If the input matches the expression, call the onMatch function
+      onMatch();
+      console.log("Matched");
+      setUserInput("");
+      setShowPopup(true);
+    } else if (showPopup) {
+      setShowPopup(false);
+    }
+  }
 
-		</>
-	)
+  // Function to convert expressions to MathML using temml then compare
+  function compareExpressions(expression: string, userInput: string) {
+    // Some manual cleaning to remove spaces and newlines
+    expression = expression.replace(/\s/g, "");
+    userInput = userInput.replace(/\s/g, "");
+    // Convert \to to \rightarrow
+    expression = expression.replace(/\\to/g, "\\rightarrow");
+    userInput = userInput.replace(/\\to/g, "\\rightarrow");
+    // TODO: Need to make some kind of conversion to make ^{\infty} and ^\infty cross-compatible
+    // userInput = userInput.replace(/\^{\\infty}/g, "^\\infty");
+    const expressionMathML = Temml.renderToString(expression);
+    const userInputMathML = Temml.renderToString(userInput);
+    return expressionMathML === userInputMathML;
+  }
+
+
+  return (
+    <>
+      <input
+        className="input"
+        type="text"
+        value={userInput}
+        onChange={handleChange}
+        placeholder="Type the expression above"
+      />
+      {showPopup && (
+        <div className="neon-text">
+          <p>Correct!</p>
+        </div>
+      )}
+      <hr />
+      <h3>Your input</h3>
+      <Katex
+        texExpression={userInput}
+        className="katex"
+      />
+
+    </>
+  )
 }
 
 
